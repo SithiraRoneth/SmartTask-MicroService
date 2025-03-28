b72534fc0bb5ae82103dd25f199297032673b452
USER <sithiraroneth@gmail.com>
2025-03-29 01:24:34 +0530

    add CRUD functions

--- task-service/src/main/java/lk/ijse/taskservice/controller/TaskController.java
+++ task-service/src/main/java/lk/ijse/taskservice/controller/TaskController.java
@@ -5,10 +5,47 @@
  * */
 package lk.ijse.taskservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.taskservice.entity.Task;
+import lk.ijse.taskservice.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/task")
 public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Task> getTaskById(@PathVariable String id) {
+        return taskService.getTaskById(id);
+    }
+
+    @GetMapping("/assigned/{userId}")
+    public List<Task> getTasksByAssignedUser(@PathVariable String userId) {
+        return taskService.getTasksByAssignedUser(userId);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable String id, @RequestBody Task task) {
+        return taskService.updateTask(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable String id) {
+        taskService.deleteTask(id);
+    }
 }
