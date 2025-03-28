259965aee5d3cb099ac87517e665b293b16ef3fa
USER <sithiraroneth@gmail.com>
2025-03-29 01:18:27 +0530

    edit task repository

--- task-service/src/main/java/lk/ijse/taskservice/repository/TaskRepo.java
+++ task-service/src/main/java/lk/ijse/taskservice/repository/TaskRepo.java
@@ -3,5 +3,10 @@ package lk.ijse.taskservice.repository;
 import lk.ijse.taskservice.entity.Task;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskRepo extends MongoRepository<Task, String> {
+    List<Task> findByAssignedTo(String assignedTo);
+    Optional<Task> findById(String id);
 }
