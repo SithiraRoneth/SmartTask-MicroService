d36105a77948cca5aa479f6958177b9b2d066c61
USER <sithiraroneth@gmail.com>
2025-03-29 01:06:56 +0530

    add eureka for connect with server

--- auth-service/Server.ts
+++ auth-service/Server.ts
@@ -2,10 +2,32 @@
 import express from 'express';
 import mongoose from "mongoose";
 import AuthController, {authenticationToken} from "./controller/AuthController";
+import {Eureka} from 'eureka-js-client';
 
 const app = express();
 const mongoUrl = "mongodb://localhost:27017/Smart-TaskManager";
-
+const eurekaClient = new Eureka({
+    instance:{
+        instanceId: "auth-service",
+        app:"AUTH-SERVICE",
+        hostName:"location",
+        ipAddr: "127.0.0.1",
+        port:{
+            $:3000,
+            "@enabled":true
+        },
+        vipAddress: "auth-service",
+        dataCenterInfo:{
+            "@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
+            name: "MyOwn"
+        }
+    },
+    eureka:{
+        host:"localhost",
+        port:8761,
+        //servicePath:"/eureka/"
+    }
+})
 mongoose
     .connect(mongoUrl)
     .then(()=>console.log("MongoDB Connected Successfully"))
@@ -17,4 +39,12 @@ app.use(authenticationToken);
 
 app.listen(3000,()=>{
     console.log("Server running port 3000");
+
+    eurekaClient.start((error) => {
+        if (error) {
+            console.log("Fail to register eureka")
+        } else {
+            console.log("successfully registered with Eureka")
+        }
+    })
 });
