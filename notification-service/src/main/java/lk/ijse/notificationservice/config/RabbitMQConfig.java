336431474c82782e972875b5a47e602c43d508f3
USER <sithiraroneth@gmail.com>
2025-03-29 15:30:56 +0530

    add RabbitMQ config

--- notification-service/src/main/java/lk/ijse/notificationservice/config/RabbitMQConfig.java
+++ notification-service/src/main/java/lk/ijse/notificationservice/config/RabbitMQConfig.java
@@ -5,5 +5,37 @@
  * */
 package lk.ijse.notificationservice.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMQConfig {
+    public static final String QUEUE_NAME = "notificationQueue";
+    public static final String EXCHANGE_NAME = "notificationExchange";
+
+    @Bean
+    Queue queue() {
+        return new Queue(QUEUE_NAME,false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("notification.key");
+    }
+
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        return new RabbitTemplate(connectionFactory);
+    }
 }
