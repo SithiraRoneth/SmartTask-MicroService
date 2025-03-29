b3b5feb8d3081a3f588d4eedd42851ef4dbeb17a
USER <sithiraroneth@gmail.com>
2025-03-29 15:36:43 +0530

    add notification service logic

--- notification-service/src/main/java/lk/ijse/notificationservice/service/NotificationService.java
+++ notification-service/src/main/java/lk/ijse/notificationservice/service/NotificationService.java
@@ -5,5 +5,21 @@
  * */
 package lk.ijse.notificationservice.service;
 
+import lk.ijse.notificationservice.dto.NotificationRequest;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import static lk.ijse.notificationservice.config.RabbitMQConfig.EXCHANGE_NAME;
+
+@Service
 public class NotificationService {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendNotification(NotificationRequest notification) {
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME,"notification.key",notification);
+        System.out.println("Notification Send: " + notification.getMessage());
+    }
 }
