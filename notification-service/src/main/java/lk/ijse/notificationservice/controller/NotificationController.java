be492c392a631b77943a714956c8adc2975224be
USER <sithiraroneth@gmail.com>
2025-03-29 15:37:58 +0530

    add notification controller logic

--- notification-service/src/main/java/lk/ijse/notificationservice/controller/NotificationController.java
+++ notification-service/src/main/java/lk/ijse/notificationservice/controller/NotificationController.java
@@ -5,10 +5,24 @@
  * */
 package lk.ijse.notificationservice.controller;
 
+import lk.ijse.notificationservice.dto.NotificationRequest;
+import lk.ijse.notificationservice.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/notification")
 public class NotificationController {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @PostMapping
+    public String sendNotification(@RequestBody NotificationRequest notification) {
+        notificationService.sendNotification(notification);
+        return "Notification sent successfully";
+    }
 }
